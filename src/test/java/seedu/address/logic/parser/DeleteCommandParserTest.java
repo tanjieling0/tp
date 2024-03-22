ebac04dfe42d1d008cb82c8e9de6f0af5774c266
Shaun Lee <shaunlxw@gmail.com>
2024-03-22 20:32:38 +0800

    bug fix: error message for Edit command
        * Add test to check accurate error message
        * Add test to check repeated id value

--- src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
@@ -51,7 +51,7 @@ public class DeleteCommandParserTest {
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, "a",
+        assertParseFailure(parser, " a",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
     }
 }
