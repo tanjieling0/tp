74aaad4f2a521030a9e9277518110765abacb7a4
Puri Virakarin <puri.prv@gmail.com>
2024-04-02 23:12:57 +0800

    Fix checkstyle main

--- src/main/java/seedu/address/model/person/filter/RemarkContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/filter/RemarkContainsKeywordsPredicate.java
@@ -1,7 +1,6 @@
 package seedu.address.model.person.filter;
 
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import seedu.address.commons.util.StringUtil;
